fdcd980e9e69b0a7ea3d02a2115155a884d34b42
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-01-06 10:48:53 -0300

    feat: add content on skills page && add components SkillsBox and Skill

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,4 +1,11 @@
+import { Props } from "astro"
+
 export interface LinkData {
     url: string
     label: string
+}
+
+export interface SkillData {
+    label: string
+    Icon: ((_props: Props) => any) & ImageMetadata
 }
