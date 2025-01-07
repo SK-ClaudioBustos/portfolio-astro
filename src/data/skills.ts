3412d39df8cc6087c0fcfd31b401e1ee3a1f7a15
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-01-06 21:51:25 -0300

    refactor: move skill item data to a external file for improve code organization && add two new skills

---
+++ src/data/skills.ts
@@ -0,0 +1,49 @@
+import AstroIcon from "@icons/astro.svg";
+import CssIcon from "@icons/css.svg";
+import GitIcon from "@icons/git.svg";
+import HtmlIcon from "@icons/html5.svg";
+import JavascriptIcon from "@icons/javascript.svg";
+import LaravelIcon from "@icons/laravel.svg";
+import PhpIcon from "@icons/php.svg";
+import ReactIcon from "@icons/react.svg";
+import TypescriptIcon from "@icons/typescript.svg";
+import { SkillData } from "@type/types";
+
+export const SKILLS: SkillData[] = [
+    {
+        label: "HTML 5",
+        Icon: HtmlIcon,
+    },
+    {
+        label: "Css",
+        Icon: CssIcon,
+    },
+    {
+        label: "Javascript",
+        Icon: JavascriptIcon,
+    },
+    {
+        label: "Typescript",
+        Icon: TypescriptIcon,
+    },
+    {
+        label: "PHP",
+        Icon: PhpIcon,
+    },
+    {
+        label: "React",
+        Icon: ReactIcon,
+    },
+    {
+        label: "Astro",
+        Icon: AstroIcon,
+    },
+    {
+        label: "Laravel",
+        Icon: LaravelIcon,
+    },
+    {
+        label: "Git",
+        Icon: GitIcon,
+    },
+];
