c481ce5b161a452f97e184b206fe51b4875915f1
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-01-09 13:09:23 -0300

    refactor: improve Navbar component

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,12 +1,12 @@
-import AstroIcon from "@icons/astro.svg";
-import CssIcon from "@icons/css.svg";
-import GitIcon from "@icons/git.svg";
-import HtmlIcon from "@icons/html5.svg";
-import JavascriptIcon from "@icons/javascript.svg";
-import LaravelIcon from "@icons/laravel.svg";
-import PhpIcon from "@icons/php.svg";
-import ReactIcon from "@icons/react.svg";
-import TypescriptIcon from "@icons/typescript.svg";
+import AstroIcon from "@icons/skills/astro.svg";
+import CssIcon from "@icons/skills/css.svg";
+import GitIcon from "@icons/skills/git.svg";
+import HtmlIcon from "@icons/skills/html5.svg";
+import JavascriptIcon from "@icons/skills/javascript.svg";
+import LaravelIcon from "@icons/skills/laravel.svg";
+import PhpIcon from "@icons/skills/php.svg";
+import ReactIcon from "@icons/skills/react.svg";
+import TypescriptIcon from "@icons/skills/typescript.svg";
 import { SkillData } from "@type/types";
 
 export const SKILLS: SkillData[] = [
