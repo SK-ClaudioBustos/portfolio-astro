577406368d3adc2fbadb977364ef350009e81001
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-05-15 22:10:43 -0300

    feat: update astro to 5.7.13

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        svg: true,
-    },
     i18n: {
         defaultLocale: "es",
         locales: ["es", "en"],
