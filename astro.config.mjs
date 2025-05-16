96f71aa2c0e1d7b03619e97ca47d3a3e378cde7b
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-05-16 12:30:56 -0300

    fix: bloqueo de tema al cambiar de lenguaje

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    prefetch: false,
     i18n: {
         defaultLocale: "es",
         locales: ["es", "en"],
